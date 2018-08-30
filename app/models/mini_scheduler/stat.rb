a2021c644b163461935be5f7f039a688a4cf4d33
Guo Xiang Tan <tgx_world@hotmail.com>
2018-08-30 09:57:34 +0800

    Allow mini_scheduler to work without ActiveRecord.

--- app/models/mini_scheduler/stat.rb
+++ app/models/mini_scheduler/stat.rb
@@ -1,10 +1,12 @@
-module MiniScheduler
-  class Stat < ActiveRecord::Base
+if defined?(ActiveRecord::Base)
+  module MiniScheduler
+    class Stat < ActiveRecord::Base
 
-    self.table_name = 'scheduler_stats'
+      self.table_name = 'scheduler_stats'
 
-    def self.purge_old
-      where('started_at < ?', 1.months.ago).delete_all
+      def self.purge_old
+        where('started_at < ?', 1.months.ago).delete_all
+      end
     end
   end
 end
